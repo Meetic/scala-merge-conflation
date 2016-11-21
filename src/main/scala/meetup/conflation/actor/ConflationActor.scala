0f6294935f451838562ed3e044a74702abf90b5a
c.verdier <c.verdier.ext@meetic-corp.com>
2016-11-21 12:11:46 +0100

    Implementation for step1 Merge Conflation

--- src/main/scala/meetup/conflation/actor/ConflationActor.scala
+++ src/main/scala/meetup/conflation/actor/ConflationActor.scala
@@ -1,16 +1,57 @@
 package meetup.conflation.actor
 
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
-import meetup.conflation.event.PriceChange
+import meetup.conflation.actor.ConflationActor.Tick
+import meetup.conflation.conflation.{Conflated, Conflation}
+import meetup.conflation.event._
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
 object ConflationActor {
 
+  case object Tick
   def props(target: ActorRef): Props = Props(new ConflationActor(target))
 }
 
-class ConflationActor(target: ActorRef) extends Actor with ActorLogging {
+class ConflationActor(target: ActorRef) extends Actor with ActorLogging with Conflation[PriceChange, MergedPriceChange] {
+
+  override val buffer = mutable.HashMap[String, Conflated[PriceChange]]()
+
+  override def bufferEvent(event: PriceChange): Unit = {
+    buffer
+      .getOrElseUpdate(event.providerId, Conflated(ListBuffer(), System.currentTimeMillis()))
+      .events.append(event)
+  }
+
+  override def mergeEvents(events: Seq[PriceChange]): MergedPriceChange = {
+    val prices: (Option[BigDecimal], Option[BigDecimal], Option[BigDecimal]) = (None, None, None)
+    val (tomatoPrice, mushroomPrice, flourPrice) = events.foldRight(prices) {
+      case (TomatoPriceChange(_, price, _, _), (None, mushroom, flour)) => (Some(price), mushroom, flour)
+      case (MushroomPriceChange(_, price, _, _), (tomato, None, flour)) => (tomato, Some(price), flour)
+      case (FlourPriceChange(_, price, _, _), (tomato, mushroom, None)) => (tomato, mushroom, Some(price))
+      case (_, all) => all
+    }
+    MergedPriceChange(events.head.providerId, tomatoPrice, mushroomPrice, flourPrice, events.last.time)
+  }
+
+  override def onTick(): Unit = {
+    log.info(s"Processing conflated events (${buffer.size})")
+    val threshold = System.currentTimeMillis() - delay.toMillis
+    buffer.retain {
+      case (_, conflated) if conflated.time < threshold =>
+        target ! mergeEvents(conflated.events)
+        false
+
+      case _ => true
+    }
+  }
 
   override def receive: Receive = {
-    case event: PriceChange => target ! event
+    case event: PriceChange => bufferEvent(event)
+    case Tick => onTick()
   }
+
+  import context.dispatcher
+  context.system.scheduler.schedule(tickInterval, tickInterval, self, Tick)
 }
